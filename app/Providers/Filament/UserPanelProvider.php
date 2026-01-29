8e265fc2b293b633e83f8caaf0207d18dd15d60a
Yohanes Wisnu Perdana <wisnuyohanes33@gmail.com>
2026-01-29 12:35:10 +0700

    fix: Add missing import for `ProtocolResource` in UserPanelProvider

--- app/Providers/Filament/UserPanelProvider.php
+++ app/Providers/Filament/UserPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\Protocols\ProtocolResource;
 use Filament\Enums\ThemeMode;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
