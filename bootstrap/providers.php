7ca59145b9c7562d07a6977e27bc39e8b03e4999
Yohanes Wisnu Perdana <wisnuyohanes33@gmail.com>
2026-03-17 09:21:32 +0700

    feat: Integrate Laravel Telescope for enhanced debugging and monitoring capabilities

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\ReviewerPanelProvider::class,
     App\Providers\Filament\UserPanelProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
